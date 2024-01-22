dbdb610da4847e77972053f27234933210ec08cf
unknown <emgcalangian@gmail.com>
2024-01-22 18:05:11 +0800

    added normal number pattern

--- downward_asterisk_pattern.py
+++ downward_asterisk_pattern.py
@@ -3,5 +3,11 @@
 # pseudo code
 
 # ask user for the desired layers of the pattern
+layers = int(input("What is your desired layer of the pyramid? "))
+
 # make the normal number pattern
+for outer in range(layers + 1):
+    for inner in range(outer):
+        print(outer, end = " ")
+    print()
 # write it so that it's only asterisks and it's downward
