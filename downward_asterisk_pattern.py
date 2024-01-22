3716a881a1cd3c5377e205006ccbcadd2dd237d8
unknown <emgcalangian@gmail.com>
2024-01-22 18:21:50 +0800

    added the code for the downward pyramid asterisks

--- downward_asterisk_pattern.py
+++ downward_asterisk_pattern.py
@@ -5,9 +5,10 @@
 # ask user for the desired layers of the pattern
 layers = int(input("What is your desired layer of the pyramid? "))
 
+
 # make the normal number pattern
-for outer in range(layers + 1):
-    for inner in range(outer):
-        print(outer, end = " ")
-    print()
 # write it so that it's only asterisks and it's downward
+for outer in range(layers,0,  -1):
+    for inner in range(1, outer + 1):
+        print(end = "*")
+    print()
