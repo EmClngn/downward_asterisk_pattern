9d87a54a3d56321a136aea3be69f145a26c116ef
unknown <emgcalangian@gmail.com>
2024-01-22 17:56:00 +0800

    pseudo code

---
+++ downward_asterisk_pattern.py
@@ -0,0 +1,7 @@
+# make a downward pyramid pattern with only asterisks
+
+# pseudo code
+
+# ask user for the desired layers of the pattern
+# make the normal number pattern
+# write it so that it's only asterisks and it's downward
